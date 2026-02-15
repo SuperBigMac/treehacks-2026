1ab69dbfeb8c14137c21872b37918eb4a291c7ff
ehtun <ehtun@stanford.edu>
2026-02-14 16:03:40 -0800

    add disconnect timeout

--- hardware/water_test/water_test.ino
+++ hardware/water_test/water_test.ino
@@ -1,16 +1,27 @@
+// '0' off, '1' on, '2' pulse, '3' heartbeat (resets disconnect timer only)
 const int signalPin = 8;
+
+// No serial for this long -> assume host disconnected -> shut off (as if "0")
+const unsigned long DISCONNECT_TIMEOUT_MS = 800;
+
+unsigned long lastReceivedMs = 0;  // 0 = never received
 
 void setup() {
   Serial.begin(9600);
   pinMode(signalPin, OUTPUT);
+  digitalWrite(signalPin, LOW);
 }
 
 void loop() {
+  // If we've received at least one byte and it's been too long, treat as disconnected
+  if (lastReceivedMs != 0 && (millis() - lastReceivedMs) > DISCONNECT_TIMEOUT_MS) {
+    digitalWrite(signalPin, LOW);
+    lastReceivedMs = 0;
+  }
 
-  // Check if data is available from serial
   if (Serial.available() > 0) {
-
     char input = Serial.read();
+    lastReceivedMs = millis();
 
     if (input == '1') {
       digitalWrite(signalPin, HIGH);
@@ -24,5 +35,6 @@ void loop() {
       delay(100);
       digitalWrite(signalPin, LOW);
     }
+    // '3' = heartbeat only (timer already updated above; no pin change)
   }
 }
